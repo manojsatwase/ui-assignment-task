e8d2c8f618b2ab3e27d451a2579ee14d883131c2
manojsatwase <33016614+manojsatwase@users.noreply.github.com>
2021-07-30 16:48:03 +0530

    Add files via upload

--- programing-task.js
+++ programing-task.js
@@ -1,4 +1,22 @@
 // example 1
+/*  
+Hello Manoj,
+
+Programming Test -: First Screening (t2021-2-1)
+Last Submission Date : 30/07/2021
+Duration: 1 day
+Level: BASIC
+
+Instructions:
+1. Create a Public GitHub Repository
+3. The test can be taken in "any programming language". Please mention coding language and comments in the README file.
+4. Once you have completed coding, reply back to the mail where you received this document, with the "URL of the GitHub Public repository".
+
+
+Problem-1: Create a small calculator which performs operations such as Addition, Subtraction, Multiplication and Division using class.
+Calculator inputs :> ‘a’, ‘b’ and ‘type of operation’
+Datatype :> ‘a’ = double, ‘b’ = double, ‘type of operation’ = string
+*/
 class Calculator {
  constructor(a, b) {
   this.a = a;
@@ -23,7 +41,23 @@ calc.substraction();
 calc.division();
 calc.multiply()
 
+
 // example 2
+/*
+
+Problem-2: With a single integer as the input, generate the following until a = x [series of numbers as shown in below examples]
+Output: (examples)
+1) input a = 1, then output : 1
+2) input a = 2, then output : 1
+3) input a = 3, then output : 1, 3, 5
+4) input a = 4, then output : 1, 3, 5
+5) input a = 5, then output : 1, 3, 5, 7, 9
+6) input a = 6, then output : 1, 3, 5, 7, 9
+.
+.
+7) input a = x, then output : 1, 3, 5, 7, .......
+
+*/
 function seriesGenerate(num) {
  if (num === 1) return num;
  let results = [];
@@ -37,7 +71,39 @@ function seriesGenerate(num) {
  return results;
 }
 seriesGenerate(5)
+
+
 // example 3
+/*
+Problem-3: With a single integer as the input, generate the following until a = x [series of numbers as shown in below examples]
+e.g. 1)
+input : 2
+output : 1
+2 , 1
+1
+
+e.g. 2)
+input : 4
+output : 1
+2 , 1
+1 , 2 , 3
+4 , 3 , 2 , 1
+1 , 2 , 3
+2 , 1
+1
+
+e.g. 3)
+input : 5
+output : 1
+2 , 1
+1 , 2 , 3
+4 , 3 , 2 , 1
+1 , 2 , 3 , 4 , 5
+4 , 3 , 2 , 1
+1 , 2 , 3
+2 , 1
+1
+*/
 function helperPatternIncOrder(input, results) {
  for (let no = 1; no <= input; no++) {
   if (no % 2 == 0) {
